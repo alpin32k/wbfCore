a8d69a3a06d4e354a7d18fedaf110707c6d6443a
Axel Andrusiewicz <alpin32k@gmail.com>
2022-12-28 17:33:47 +0100

    update HelpCommand.java

--- src/alpine/wbf/core/commands/HelpCommand.java
+++ src/alpine/wbf/core/commands/HelpCommand.java
@@ -13,21 +13,23 @@ public class HelpCommand implements CommandExecutor {
         if(sender instanceof Player)
         {
 
-            MessageUtils.sendMessage((Player) sender, "");
-            MessageUtils.sendMessage((Player) sender, "                 &c&l-=| PLAYER SECTION |=-");
-            MessageUtils.sendMessage((Player) sender, "");
-            MessageUtils.sendMessage((Player) sender, "&6/help        &7- tutaj jesteś");
-            MessageUtils.sendMessage((Player) sender, "&6/spawn      &7- teleport na spawn (nie możesz się ruszać)");
-            MessageUtils.sendMessage((Player) sender, "&6/craft      &7- otwiera przenośny crafting table");
-            MessageUtils.sendMessage((Player) sender, "");
-            MessageUtils.sendMessage((Player) sender, "");
+            MessageUtils.sendMessage(sender, "");
+            MessageUtils.sendMessage(sender, "                 &c&l-=| PLAYER SECTION |=-");
+            MessageUtils.sendMessage(sender, "");
+            MessageUtils.sendMessage(sender, "&6/help        &7- tutaj jesteś");
+            MessageUtils.sendMessage(sender, "&6/spawn      &7- teleport na spawn (nie możesz się ruszać)");
+            MessageUtils.sendMessage(sender, "&6/craft      &7- otwiera przenośny crafting table");
+            MessageUtils.sendMessage(sender, "");
+            MessageUtils.sendMessage(sender, "");
 
             if(sender.hasPermission("wbfCore.admin")){
-                MessageUtils.sendMessage((Player) sender, "");
-                MessageUtils.sendMessage((Player) sender, "                 &c&l-=| ADMIN SECTION |=-");
-                MessageUtils.sendMessage((Player) sender, "&c/openinv      &3<player>&7- otwiera ekwipunek gracza");
-                MessageUtils.sendMessage((Player) sender, "&c/openechest  &3<player>&7- otwiera enderchest gracza");
-                MessageUtils.sendMessage((Player) sender, "");
+                MessageUtils.sendMessage(sender, "");
+                MessageUtils.sendMessage(sender, "                 &c&l-=| ADMIN SECTION |=-");
+                MessageUtils.sendMessage(sender, "&c/openinv        &3<player>&7- otwiera ekwipunek gracza");
+                MessageUtils.sendMessage(sender, "&c/openechest   &3<player>&7- otwiera enderchest gracza");
+                MessageUtils.sendMessage(sender, "&c/time   &3<day/noon/night>&7- ustawia godzinę na serwerze");
+                MessageUtils.sendMessage(sender, "&c/weather   &3<clear/rain>&7- ustawia pogodę na serwerze");
+                MessageUtils.sendMessage(sender, "");
             }
 
             return true;
